47b55ebc55c1abc4dfe83451e4bb8877c42b94b6
Аяулым <aaulym@MacBook-Pro-Aaulym-2.local>
2023-02-12 18:26:31 +0600

    start

--- src/any_source.go
+++ src/any_source.go
@@ -1,1 +1,25 @@
+package main
 
+import "fmt"
+
+func main() {
+    var nameOfStore = "Nike store"
+    fmt.Println("Welcome to our", nameOfStore, "!")
+    fmt.Println("Register to continue: ")
+
+    var userName string
+    var userSurname string
+    var userEmail string
+    var userPassword string
+
+    fmt.Println("Enter your name: ")
+    fmt.Scan(&userName)
+    fmt.Println("Enter your surname: ")
+    fmt.Scan(&userSurname)
+    fmt.Println("Enter your email: ")
+    fmt.Scan(&userEmail)
+    fmt.Println("Create a password: ")
+    fmt.Scan(&userPassword)
+
+    fmt.Printf("Hello, %v %v! Your account is created.\n", userName, userSurname)
+}
