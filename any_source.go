f5fc18f8f573cdbdfb7f3833a892645326e7cd67
Аяулым <aaulym@MacBook-Pro-Aaulym-2.local>
2023-02-12 17:39:33 +0600

    project report1

--- any_source.go
+++ any_source.go
@@ -3,10 +3,23 @@ package main
 import "fmt"
 
 func main() {
-  message := greetMe("world")
-  fmt.Println(message)
-}
+    var nameOfStore = "Nike store"
+    fmt.Println("Welcome to our", nameOfStore, "!")
+    fmt.Println("Register to continue: ")
 
-func greetMe(name string) string {
-  return "Hello, " + name + "!"
+    var userName string
+    var userSurname string
+    var userEmail string
+    var userPassword string
+
+    fmt.Println("Enter your name: ")
+    fmt.Scan(&userName)
+    fmt.Println("Enter your surname: ")
+    fmt.Scan(&userSurname)
+    fmt.Println("Enter your email: ")
+    fmt.Scan(&userEmail)
+    fmt.Println("Create a password: ")
+    fmt.Scan(&userPassword)
+
+    fmt.Printf("Hello, %v %v! Your account is created.\n", userName, userSurname)
 }
