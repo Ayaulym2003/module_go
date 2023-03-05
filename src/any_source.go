db19c2052c6f6210175be317741d2842c18122d3
Аяулым <aaulym@MacBook-Pro-Aaulym-2.local>
2023-03-05 23:26:04 +0600

    Progress report 4

--- src/any_source.go
+++ src/any_source.go
@@ -49,13 +49,47 @@ func save_reg(w http.ResponseWriter, r *http.Request){
 
     http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+func save_log(w http.ResponseWriter, r *http.Request){
+     t, err2 := template.ParseFiles("templates/login.html", "templates/index.html")
+       if err2 != nil {
+            fmt.Fprintf(w, err2.Error())
+       }
+    email := r.FormValue("email")
+    password := r.FormValue("password")
+    fmt.Println("email:", email, "password:", password)
 
+    db, err := sql.Open("mysql", "root:12345678@tcp(127.0.0.1:3306)/golang")
+    if err != nil {
+        panic(err)
+    }
+    defer db.Close()
+
+    var hash string
+    stmt := "SELECT password FROM register WHERE email = ?"
+    row := db.QueryRow(stmt, email)
+    erro := row.Scan(&hash)
+    fmt.Println("hash:", hash)
+
+    if erro != nil {
+     fmt.Println("Ayaaau")
+     t.ExecuteTemplate(w, "login", "check email and password")
+     return
+    }
+    if hash == password {
+     t.ExecuteTemplate(w, "index", "You have successfully logged in!")
+     return
+    }
+    fmt.Println("incorrect password")
+    t.ExecuteTemplate(w, "login", "check email and password")
+
+}
 func handleFunc (){
     http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
     http.HandleFunc("/", index)
     http.HandleFunc("/login", login)
     http.HandleFunc("/register", register)
     http.HandleFunc("/save_reg", save_reg)
+    http.HandleFunc("/save_log", save_log)
     http.ListenAndServe(":8080", nil)
 }
 func main() {
