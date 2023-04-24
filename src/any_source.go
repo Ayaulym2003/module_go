7c48b3bf328a5993ae8144a4d631cd19c93dada1
Аяулым <aaulym@MacBook-Pro-Aaulym-2.local>
2023-04-25 03:37:04 +0600

    Midter2

--- src/any_source.go
+++ src/any_source.go
@@ -24,6 +24,13 @@ type Products struct {
     Description string
     Photo string
 }
+
+type Comments struct {
+    Comment_id int64
+    Product_id int64
+    Comment string
+}
+
 type Customer struct {
     Customer_id int64
     Name string
@@ -61,7 +68,7 @@ func desc(w http.ResponseWriter, r *http.Request){
             fmt.Fprintf(w, "HHKJ")
        }
 
-       t, err := template.ParseFiles("templates/desc.html", "templates/header.html")
+       t, err := template.ParseFiles("templates/desc.html")
                if err != nil {
                     fmt.Fprintf(w, err.Error())
                }
@@ -88,19 +95,41 @@ func desc(w http.ResponseWriter, r *http.Request){
                   prods = append(prods, P)
                }
 
-               t.ExecuteTemplate(w, "desc", prods)
+              s, err := db.Query(fmt.Sprintf("SELECT * FROM `comment` WHERE `product_id` = %d", y))
+                     if err != nil {
+                     fmt.Fprintf(w, err.Error())
+                     return
+             }
+             defer s.Close()
+
+             var prod []Comments
+                  for s.Next() {
+                  var C Comments
+                  err = s.Scan(&C.Comment_id, &C.Product_id, &C.Comment)
+                        if err != nil {
+                             panic(err)
+                        }
+                   prod = append(prod, C)
+             }
+
+             fmt.Println(prod)
+             x := make(map[string]interface{})
+             x["prods"] = prods
+             x["prod"] = prod
+
+             t.ExecuteTemplate(w, "desc", x)
   }
 
 
 func index(w http.ResponseWriter, r *http.Request){
-       t, err := template.ParseFiles("templates/index.html", "templates/header.html")
+       t, err := template.ParseFiles("templates/index.html")
        if err != nil {
             fmt.Fprintf(w, err.Error())
-       }
+      }
       t.ExecuteTemplate(w, "index", nil)
  }
 func filter(w http.ResponseWriter, r *http.Request){
-       t, err := template.ParseFiles("templates/filter.html",  "templates/header.html")
+       t, err := template.ParseFiles("templates/filter.html")
                if err != nil {
                     fmt.Fprintf(w, err.Error())
                }
@@ -130,7 +159,7 @@ func filter(w http.ResponseWriter, r *http.Request){
                t.ExecuteTemplate(w, "filter", prods)
   }
  func products(w http.ResponseWriter, r *http.Request){
-       t, err := template.ParseFiles("templates/products.html", "templates/header.html")
+       t, err := template.ParseFiles("templates/products.html")
         if err != nil {
              fmt.Fprintf(w, err.Error())
         }
@@ -162,7 +191,7 @@ func filter(w http.ResponseWriter, r *http.Request){
   }
 
 func save_reg(w http.ResponseWriter, r *http.Request){
-    t, err2 := template.ParseFiles("templates/register.html",  "templates/header.html")
+    t, err2 := template.ParseFiles("templates/register.html")
        if err2 != nil {
             fmt.Fprintf(w, err2.Error())
        }
@@ -189,7 +218,7 @@ func save_reg(w http.ResponseWriter, r *http.Request){
     http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 func save_log(w http.ResponseWriter, r *http.Request){
-     t, err2 := template.ParseFiles("templates/login.html", "templates/index.html", "templates/header.html")
+     t, err2 := template.ParseFiles("templates/login.html", "templates/index.html")
        if err2 != nil {
             fmt.Fprintf(w, err2.Error())
        }
@@ -216,11 +245,9 @@ func save_log(w http.ResponseWriter, r *http.Request){
     }
 
     if hash == Password {
-
      session, _ := store.Get(r, "session")
-     session.Values["customer_id"] = true
+     session.Values["email"] = Email
      session.Save(r, w)
-     t.Lookup("header")
      t.ExecuteTemplate(w, "index", Email)
      return
     }
@@ -230,7 +257,7 @@ func save_log(w http.ResponseWriter, r *http.Request){
 }
 
 func search(w http.ResponseWriter, r *http.Request){
-        t, err := template.ParseFiles("templates/products.html",  "templates/header.html")
+        t, err := template.ParseFiles("templates/products.html")
         if err != nil {
              fmt.Fprintf(w, err.Error())
         }
@@ -264,7 +291,7 @@ func search(w http.ResponseWriter, r *http.Request){
   }
 
  func filter_filter(w http.ResponseWriter, r *http.Request){
-         t, err := template.ParseFiles("templates/filter.html",  "templates/header.html")
+         t, err := template.ParseFiles("templates/filter.html")
          if err != nil {
               fmt.Fprintf(w, err.Error())
          }
@@ -300,7 +327,179 @@ func search(w http.ResponseWriter, r *http.Request){
          t.ExecuteTemplate(w, "filter", prod)
    }
 
+func test(w http.ResponseWriter, r *http.Request){
+     session, _ := store.Get(r, "session")
+     untyped, ok := session.Values["email"]
+     if !ok {
+       return
+     }
+     email, ok := untyped.(string)
+     if !ok {
+            return
+          }
+     w.Write([]byte(email))
+}
 
+func comment(w http.ResponseWriter, r *http.Request){
+     vars := mux.Vars(r)
+     y, e := strconv.Atoi(vars["Product_id"])
+     if e != nil {
+            fmt.Fprintf(w, "HHKJ")
+     }
+     comment := r.FormValue("comment")
+
+     t, err := template.ParseFiles("templates/desc.html")
+       if err != nil {
+              fmt.Fprintf(w, err.Error())
+        }
+       db, err := sql.Open("mysql", "root:12345678@tcp(127.0.0.1:3306)/golang")
+            if err != nil {
+                  panic(err)
+            }
+            defer db.Close()
+
+         sel, err := db.Query(fmt.Sprintf("SELECT * FROM `product` WHERE `product_id` = %d", y))
+         if err != nil {
+         fmt.Fprintf(w, err.Error())
+          return
+             }
+            defer sel.Close()
+
+         var prods []Products
+          for sel.Next() {
+            var P Products
+            err = sel.Scan(&P.Product_id, &P.Category_id, &P.Product_name, &P.Price, &P.Size, &P.Color, &P.Rating, &P.Description, &P.Photo)
+        if err != nil {
+              panic(err)
+        }
+        prods = append(prods, P)
+           }
+
+        insert, err := db.Query(fmt.Sprintf("Insert into `comment` (`product_id`, `comment`) Values ('%d', '%s')", y, comment))
+            if err != nil {
+                 fmt.Println("Ayaaau")
+                 t.ExecuteTemplate(w, "desc", "something is not right")
+                 return
+        }
+        defer insert.Close()
+
+         s, err := db.Query(fmt.Sprintf("SELECT * FROM `comment` WHERE `product_id` = %d", y))
+                             if err != nil {
+                             fmt.Fprintf(w, err.Error())
+                             return
+                     }
+                     defer s.Close()
+
+                     var prod []Comments
+                          for s.Next() {
+                          var C Comments
+                          err = s.Scan(&C.Comment_id, &C.Product_id, &C.Comment)
+                                if err != nil {
+                                     panic(err)
+                                }
+                           prod = append(prod, C)
+                     }
+                     x := make(map[string]interface{})
+                     x["prods"] = prods
+                     x["prod"] = prod
+
+                     t.ExecuteTemplate(w, "desc", x)
+
+}
+
+  func rate(w http.ResponseWriter, r *http.Request){
+       vars := mux.Vars(r)
+       y, e := strconv.Atoi(vars["Product_id"])
+       if e != nil {
+              fmt.Fprintf(w, "HHKJ")
+       }
+          t, err := template.ParseFiles("templates/desc.html")
+          if err != nil {
+               fmt.Fprintf(w, err.Error())
+          }
+          rating := r.FormValue("rating")
+
+          db, err := sql.Open("mysql", "root:12345678@tcp(127.0.0.1:3306)/golang")
+                 if err != nil {
+                    panic(err)
+                 }
+              defer db.Close()
+
+
+          rows, err := db.Query("SELECT rating FROM product WHERE product_id = ?;", y)
+
+          if err != nil {
+               fmt.Fprintf(w, err.Error())
+               return
+          }
+          defer rows.Close()
+
+          var prod []Products
+          for rows.Next() {
+          var P Products
+                err = rows.Scan(&P.Rating)
+                if err != nil {
+                         panic(err)
+                     }
+               prod = append(prod, P)
+               }
+
+          i, err := strconv.Atoi(rating)
+              if err != nil {
+                  panic(err)
+              }
+
+          i = (i + prod[0].Rating)/2.0
+
+          row, er := db.Query("UPDATE product SET rating = ? WHERE product_id = ?", i, y)
+
+          if er != nil {
+                fmt.Fprintf(w, er.Error())
+                return
+          }
+          defer row.Close()
+
+          sel, err := db.Query(fmt.Sprintf("SELECT * FROM `product` WHERE `product_id` = %d", y))
+                   if err != nil {
+                   fmt.Fprintf(w, err.Error())
+                    return
+                       }
+                      defer sel.Close()
+
+                   var prods []Products
+                    for sel.Next() {
+                      var P Products
+                      err = sel.Scan(&P.Product_id, &P.Category_id, &P.Product_name, &P.Price, &P.Size, &P.Color, &P.Rating, &P.Description, &P.Photo)
+                  if err != nil {
+                        panic(err)
+                  }
+             prods = append(prods, P)
+             }
+
+           s, err := db.Query(fmt.Sprintf("SELECT * FROM `comment` WHERE `product_id` = %d", y))
+                                       if err != nil {
+                                       fmt.Fprintf(w, err.Error())
+                                       return
+                               }
+                               defer s.Close()
+
+                               var proda []Comments
+                                    for s.Next() {
+                                    var A Comments
+                                    err = s.Scan(&A.Comment_id, &A.Product_id, &A.Comment)
+                                          if err != nil {
+                                               panic(err)
+                                          }
+                                     proda = append(proda, A)
+                               }
+
+                               fmt.Println(proda)
+                               x := make(map[string]interface{})
+                               x["prods"] = prods
+                               x["prod"] = proda
+
+                               t.ExecuteTemplate(w, "desc", x)
+    }
 
 func handleFunc (){
     r := mux.NewRouter()
@@ -315,6 +514,11 @@ func handleFunc (){
     r.HandleFunc("/filter_filter", filter_filter)
     r.HandleFunc("/save_log", save_log)
     r.HandleFunc("/search", search)
+    r.HandleFunc("/comment/{Product_id}", comment)
+    r.HandleFunc("/rate/{Product_id}", rate)
+    r.HandleFunc("/test", test)
+
+
 
     fileServer := http.FileServer(http.Dir("./static/"))
     r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fileServer))
